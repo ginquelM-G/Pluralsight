2c2d07613b2560c8b2844a972e891efde0d5e73d
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-10-26 15:25:36 +0100

    Debugging via sourcemap... DONE

--- Nodejs/JS-DEV-ENV-DEMO/buildScripts/startMessage.js
+++ Nodejs/JS-DEV-ENV-DEMO/buildScripts/startMessage.js
@@ -5,4 +5,3 @@ import chalk from 'chalk'; // allow to set de text color of output - ES6
 
 console.log(chalk.green('Starting app in dev mode...'));
 
-/** A source-to-source compiler, transcompiler or transpiler is a type of compiler that takes the source code of a program written in one programming language as its input and produces the equivalent source code in another programming language */
