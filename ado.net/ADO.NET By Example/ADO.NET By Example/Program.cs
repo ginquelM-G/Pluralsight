3eedb92fb66219e8969923878af61d5392fb0633
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-12-05 14:50:56 +0000

    *** Establishing Connections with the Database ***

---
+++ ado.net/ADO.NET By Example/ADO.NET By Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO.NET_By_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=SI2_Trabalho_1718i;Data Source=GINQUELM-E\\SQLSERVER";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            Console.WriteLine("Connection Establish Secuessfully...");
+            Console.ReadKey();
+        }
+
+    }
+}
