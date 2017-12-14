74b739ee35d107b346f7adba5bf71020e770edc3
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-12-14 14:10:54 +0000

    companion, properties, data class

--- kotlin/Projects/classesandobjects/src/main/kotlin/com/rsk/Person.kt
+++ kotlin/Projects/classesandobjects/src/main/kotlin/com/rsk/Person.kt
@@ -9,34 +9,64 @@ interface Signatory{
     fun sign()
 }
 
+
 //Open - para a classe deixar de ser final
 open class Person(val name: String, var age:Int, var isMarried: Boolean=false): Signatory {
 
-    /*var isMarried = false
-
-    //Secondary constructor
-    constructor(name: String, age: Int, isMarried: Boolean): this(name, age){
-        this.isMarried = isMarried
-    }*/
-    /*init{
-        if(name == "Ginquel" && age < 54) throw Exception("Invalid age")
-    }*/
-
-
+    var partnerName: String = ""
 
     override fun sign() = println("$name aged $age can sign documents. (and their marital status is $isMarried)")
+
+
+    //Tipo static em java
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>){
+            val p = Person("Pedro", 23, true)
+            p.sign()
+
+            p.age = 54
+            p.sign()
+
+            p.partnerName = "Alice"
+            println("Partner name is ${p.partnerName}")
+
+            p.partnerName = "Maria"
+            println("Partner name is ${p.partnerName}")
+
+            val kevin = User("kevin", 17)
+            println(kevin)
+        }
+    }
 }
 
 class Student (name: String, age: Int) : Person(name, age)
 
+// DATA Classes - class to only hold data
+data class User(val name:String, val id: Int)
+
 
 
 fun main(args: Array<String>){
-    val p = Person("Ginquel", 23, true)
+    val p = Person("Pedro", 23, true)
     p.sign()
 
     p.age = 54
     p.sign()
 
-    p
+    p.partnerName = "Alice"
+    println("Partner name is ${p.partnerName}")
+
+    p.partnerName = "Maria"
+    println("Partner name is ${p.partnerName}")
+
+    val kevin = User("kevin", 17)
+    val otherUser = kevin.copy(id=2)
+    println(kevin)
+
+    val (name, id) = kevin
+
+    println("user is $name and their id is $id")
+    println(otherUser)
 }
