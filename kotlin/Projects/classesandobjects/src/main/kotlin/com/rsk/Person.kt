9913679c7fe8926424d89a4c3a2654340065fa7f
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-12-14 13:55:20 +0000

    Interface and classes

--- kotlin/Projects/classesandobjects/src/main/kotlin/com/rsk/Person.kt
+++ kotlin/Projects/classesandobjects/src/main/kotlin/com/rsk/Person.kt
@@ -5,3 +5,38 @@ package com.rsk
  */
 
 
+interface Signatory{
+    fun sign()
+}
+
+//Open - para a classe deixar de ser final
+open class Person(val name: String, var age:Int, var isMarried: Boolean=false): Signatory {
+
+    /*var isMarried = false
+
+    //Secondary constructor
+    constructor(name: String, age: Int, isMarried: Boolean): this(name, age){
+        this.isMarried = isMarried
+    }*/
+    /*init{
+        if(name == "Ginquel" && age < 54) throw Exception("Invalid age")
+    }*/
+
+
+
+    override fun sign() = println("$name aged $age can sign documents. (and their marital status is $isMarried)")
+}
+
+class Student (name: String, age: Int) : Person(name, age)
+
+
+
+fun main(args: Array<String>){
+    val p = Person("Ginquel", 23, true)
+    p.sign()
+
+    p.age = 54
+    p.sign()
+
+    p
+}
